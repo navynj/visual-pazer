0950bf801d3a8c45f914afdcd44558607a31c950
earlyoonj <earlyoonj@gmail.com>
2021-06-06 18:27:50 +0900

    Integrate with SwiftUI : UIViewControllerRepresentable

---
+++ VisualPazer/VisualPazer/VisualPazerApp.swift
@@ -0,0 +1,23 @@
+//
+//  VisualPazerApp.swift
+//  VisualPazer
+//
+//  Created by 이윤지 on 2021/06/06.
+//
+
+import SwiftUI
+
+@main
+struct VisualPazerApp: App {
+    
+//    @StateObject var gazePoint: GazeViewModel = GazeViewModel()
+//    @StateObject let tracker: TrackerViewModel = TrackerViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            SideMenuView()
+//                .environmentObject(gazePoint)
+//                .environmentObject(tracker)
+        }
+    }
+}
