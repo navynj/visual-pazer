d932f59567840535fb29040925a10da5a1110abe
earlyoonj <earlyoonj@gmail.com>
2021-06-06 18:23:56 +0900

    Finish studying sample

---
+++ VisualPazer/VisualPazer/views/CallibrationPointView.swift
@@ -0,0 +1,34 @@
+//
+//  CallibrationPointView.swift
+//  VisualPazer
+//
+//  Created by 이윤지 on 2021/06/05.
+//
+
+import UIKit
+
+class CalibrationPointView : UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame : frame)
+        layer.cornerRadius = frame.width/2
+        layer.borderColor = UIColor.red.cgColor
+        layer.borderWidth = 2
+        textAlignment = .center
+        textColor = .red
+        adjustsFontSizeToFitWidth = true
+        text = "0%"
+    }
+    
+    func setProgress(progress : Double) {
+        let percent = Int(progress * 100)
+        DispatchQueue.main.async {
+            self.text = "\(percent)%"
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
